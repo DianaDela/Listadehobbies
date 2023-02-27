ae7e0eeb1ca3514d2667a22c44bdc730fd256956
Diana Itzel De la o Veracruz <l19350640@tuxtepec.tecnm.mx>
2023-02-27 15:11:40 -0600

    lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name= "Diana Itzel de la o Veracruz";
+  return (
+    <div>
+      <h3>Lista de Hobbies de {name}</h3>
+      <ul>
+        <li>Tocar la guitarra</li>
+        <li>Jugar videojuegos</li>
+        <li>cocinar</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
